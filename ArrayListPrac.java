a8c3a9d973f00d7207e67e2aaf4bd988003ded18
Sampat Pachade <sericpachade@gmail.com>
2018-11-27 08:37:33 -0500

    New ArrayList
    Two ArrayLists created in java file called ArrayListprac

---
+++ ArrayListPrac.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class ArrayListPrac{
+
+	public static void main(String []args){
+		ArrayList <Integer> list1 = new ArrayList<>();
+		ArrayList <Integer> list2 = new ArrayList<>();
+
+	}
+}
