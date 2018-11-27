11b52ce663928cb555d84b05d3265e9f882c0e82
Sampat Pachade <sericpachade@gmail.com>
2018-11-27 08:43:49 -0500

    ArrayLists Filled
    Both ArrayLists are filled with numbers between 1 and 10

--- ArrayListPrac.java
+++ ArrayListPrac.java
@@ -6,5 +6,10 @@ public class ArrayListPrac{
 		ArrayList <Integer> list1 = new ArrayList<>();
 		ArrayList <Integer> list2 = new ArrayList<>();
 
+		list1.add(1); list2.add(6);
+		list1.add(2); list2.add(7);
+		list1.add(3); list2.add(8);
+		list1.add(4); list2.add(9);
+		list1.add(5); list2.add(10);
 	}
 }
