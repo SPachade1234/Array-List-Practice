bf591efe9266a2640423663e9293a9d5b19f9c12
Sampat Pachade <sericpachade@gmail.com>
2018-11-27 09:04:02 -0500

    New Changes
    New method created to print array list without brackets. New ArrayList created and filled with combined ArrayList. New output statements added.

--- ArrayListPrac.java
+++ ArrayListPrac.java
@@ -5,6 +5,8 @@ public class ArrayListPrac{
 	public static void main(String []args){
 		ArrayList <Integer> list1 = new ArrayList<>();
 		ArrayList <Integer> list2 = new ArrayList<>();
+		ArrayList <Integer> list3 = new ArrayList<>();
+
 
 		list1.add(1); list2.add(6);
 		list1.add(2); list2.add(7);
@@ -12,11 +14,12 @@ public class ArrayListPrac{
 		list1.add(4); list2.add(9);
 		list1.add(5); list2.add(10);
 
-		System.out.println( combineLists(list1, list2) );
+		System.out.println( combineLists(list1, list2, list3) );
+
+
 	}
 
-	public static ArrayList<Integer> combineLists(ArrayList<Integer> list1, ArrayList<Integer> list2){
-		ArrayList<Integer> list3 = new ArrayList<>();
+	public static ArrayList<Integer> combineLists(ArrayList<Integer> list1, ArrayList<Integer> list2, ArrayList<Integer> list3){
 
 		for(int i = 0; i < list1.size(); i++)
 			list3.add(list1.get(i));
@@ -25,5 +28,10 @@ public class ArrayListPrac{
 			list3.add(list2.get(i));
 
 		return list3;
+	}
+
+	public static void printArrayList(ArrayList<Integer> list){
+		for(int i = 0; i < list.size(); i++)
+			System.out.print(list.get(i) + " , ");
 	}
 }
